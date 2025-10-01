498fbd0d0a5b18eee5fb7a2408547e51c0629549
alumno <alumno@Instituto.local>
2025-10-01 08:26:15 -0300

    batmanapi

---
+++ index.js
@@ -0,0 +1,5 @@
+//Personaje random
+const tituloPersonaje = document.querySelector('#title1')
+const imagenPersonaje = document.querySelector('#character')
+const parrafoPersonaje = document.querySelector('#paragraph1')
+const botonPersonaje = document.querySelector('#button1')
