1c8edd025192e031ea490e3f3bd757c934952190
GonzaSai <136342939+GonzaSai@users.noreply.github.com>
2025-10-02 10:55:01 -0300

    batmanapi

--- index.js
+++
@@ -1,5 +0,0 @@
-//Personaje random
-const tituloPersonaje = document.querySelector('#title1')
-const imagenPersonaje = document.querySelector('#character')
-const parrafoPersonaje = document.querySelector('#paragraph1')
-const botonPersonaje = document.querySelector('#button1')
