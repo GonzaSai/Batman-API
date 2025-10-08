e270f7b4f17e4ea1b600cfa2c0f54e44862c79d0
alumno <alumno@Instituto.local>
2025-10-08 08:27:32 -0300

    batmanapi

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 const tituloPersonaje = document.querySelector('#title1')
 const parrafoPersonaje = document.querySelector('#paragraph1')
 const botonPersonaje = document.querySelector('#button1')
-const personajes = document.querySelector('#characters')
+const contenedorPersonajes = document.querySelector('#characters1')
+const idPersonaje = document.querySelector('#number')
 
 botonPersonaje.onclick = function () {
-    fetch(`https://api.batmanapi.com/v1/characters`)
+    fetch(`https://api.batmanapi.com/v1/characters/${idPersonaje.value}`)
     .then(res => res.json())
-    .then(imagenPersonaje => {
-        tituloPersonaje.textContent = imagenPersonaje.name
+    .then(personaje => {
+       console.log(personaje)
+        tituloPersonaje.textContent = personaje.data.attributes.name;
+        contenedorPersonajes.innerHTML = <h1>${personaje.data.attributes.alias}</h1>
+        parrafoPersonaje.textContent = personaje.description
     });
 
 }
